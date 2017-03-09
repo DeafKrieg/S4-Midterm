a915e1fe0c3e9df8578a1debd7bda9981a3c8308
Dave Kreke <gavtheruiner@gmail.com>
2017-03-08 20:29:35 -0600

    Update CIf.java

--- CIf.java
+++ CIf.java
@@ -8,16 +8,18 @@ public class CIf {
 		double num3 = 10;
 		double num4 = 20;
 		
+			//if
 		if(num1 == num2){
 			System.out.println("The first and second numbers are equal.");
 		}
 		
+			//if else
 		if(num2 < num3){
 			System.out.println("The second number is less than the third.");
 		} else{
 			System.out.println("The second number is not less than the third.");
 		}
-		
+			// if else if
 		if(num2 < num3){
 			System.out.println("The second number is less than the third.");
 		} else if(num2 > num3){
